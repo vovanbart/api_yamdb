314076fb93f3eedb4cfb98fa7db50671fbd1e451
AngelNad <angelnad@yandex.ru>
2021-12-15 00:51:12 +0300

    Merge models-categ-genres-titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,89 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название')
+    year = models.IntegerField(
+        validators=[MaxValueValidator(timezone.now().year)],
+        verbose_name='Год'
+    )
+    description = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name='Описание'
+    )
+    genre = models.ManyToManyField(
+        'Genre',
+        through='GenreTitle',
+        related_name='genre',
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='category',
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        'Genre',
+        on_delete=models.CASCADE
+    )
+    title = models.ForeignKey(
+        'Title',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.title} {self.genre}'
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):
