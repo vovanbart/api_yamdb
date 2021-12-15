15b74a29c8443adeb1e6994dc70a35a52fa943ae
vovanbart <vovanbart@icloud.com>
2021-12-15 21:54:27 +0300

    mistakes

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Comment, Review
+from .models import Comment, Review, Title, Genre, Category
 
 admin.site.register(Review)
 admin.site.register(Comment)
+admin.site.register(Title)
+admin.site.register(Genre)
+admin.site.register(Category)
--- api_yamdb/reviews/management/commands/data_loading.py
+++ api_yamdb/reviews/management/commands/data_loading.py
@@ -3,7 +3,7 @@ import csv
 from django.conf import settings
 from django.core.management import BaseCommand
 
-from reviews.models import (Category, Comment, Genre, GenreTitle, Review,
+from api_yamdb.reviews.models import (Category, Comment, Genre, GenreTitle, Review,
                                       Title)
 
 TABLES_DICT = {
