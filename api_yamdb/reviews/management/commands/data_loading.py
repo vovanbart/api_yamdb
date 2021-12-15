4301ac0a66165fe98b286da1dcb8a5ee6ffd8101
vovanbart <vovanbart@icloud.com>
2021-12-15 14:36:03 +0300

    10

---
+++ api_yamdb/reviews/management/commands/data_loading.py
@@ -0,0 +1,32 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from api_yamdb.reviews.models import (Category, Comment, Genre, GenreTitle, Review,
+                                      Title, User)
+
+TABLES_DICT = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
+    GenreTitle: 'genre_title.csv'
+}
+
+
+class Command(BaseCommand):
+    help = 'Load csv data'
+
+    def handle(self, *args, **kwargs):
+        for model, base in TABLES_DICT.items():
+            with open(
+                f'{settings.BASE_DIR}/static/data/{base}',
+                'r', encoding='UTF-8'
+            ) as csv_file:
+                reader = csv.DictReader(csv_file)
+                model.objects.bulk_create(model(**data) for data in reader)
+
+        self.stdout.write(self.style.SUCCESS('Successfully'))
