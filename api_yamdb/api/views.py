c4711f031d9ac121e8095dd1662daccb68c85891
vovanbart <vovanbart@icloud.com>
2021-12-15 17:07:34 +0300

    views urls serializers

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,123 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from reviews.models import Category, Genre, Review, Title
+
+from .filters import TitleFilter
+from .permissions import (IsAuthorOrReadOnly,
+                          IsModerator,
+                          IsAdmin,
+                          ReadOnly)
+from .serializers import (CategorySerializer,
+                          CommentSerializer,
+                          GenreSerializer,
+                          ReviewSerializer,
+                          TitleCreateSerializer,
+                          TitleSerializer)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """
+    Admin manage titles, other read
+    """
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdmin | ReadOnly,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH',):
+            return TitleCreateSerializer
+        return TitleSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    Admin, Moderator manage reviews
+    User manage self reviews
+    """
+    serializer_class = ReviewSerializer
+    permission_classes = (
+        IsAdmin | IsModerator | IsAuthorOrReadOnly,
+    )
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Admin, Moderator manage comments
+    User manage self comments
+    """
+    serializer_class = CommentSerializer
+    permission_classes = (
+        IsAdmin | IsModerator | IsAuthorOrReadOnly,
+    )
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """
+    Admin can manage genres, other can only read
+    """
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdmin | ReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+    @action(
+        detail=False,
+        methods=['delete'],
+        url_path=r'(?P<slug>\w+)',
+        lookup_field='slug',
+        url_name='category_slug',
+    )
+    def get_genre(self, request, slug):
+        category = self.get_object()
+        serializer = CategorySerializer(category)
+        category.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    Admin can manage genres, other can only read
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdmin | ReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+    @action(
+        detail=False,
+        methods=['delete'],
+        url_path=r'(?P<slug>\w+)',
+        lookup_field='slug',
+        url_name='category_slug',
+    )
+    def get_category(self, request, slug):
+        category = self.get_object()
+        serializer = CategorySerializer(category)
+        category.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
