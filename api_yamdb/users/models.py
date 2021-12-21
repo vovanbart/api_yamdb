e5677816cfab43695643159c4524c2c58da635bd
vovanbart <vovanbart@icloud.com>
2021-12-21 12:11:03 +0300

    ready

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,33 +3,35 @@ from django.db import models
 
 
 class User(AbstractUser):
-    class UserRole:
-        USER = 'user'
-        MODERATOR = 'moderator'
-        ADMIN = 'admin'
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
 
     ROLES = (
-        (UserRole.USER, 'user'),
-        (UserRole.MODERATOR, 'moderator'),
-        (UserRole.ADMIN, 'admin'),
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
     )
 
-    bio = models.TextField('Биография', blank=True,)
-    role = models.CharField('Роль', max_length=10,
+    bio = models.TextField('Биография',
+                           blank=True,)
+    role = models.CharField('Роль',
+                            max_length=10,
                             choices=ROLES,
-                            default=UserRole.USER)
-    email = models.EmailField('E-mail', unique=True,)
+                            default=USER)
+    email = models.EmailField('E-mail',
+                              unique=True,)
 
     @property
     def is_admin(self):
-        return self.role == self.UserRole.ADMIN or self.is_superuser
+        return self.role == self.ADMIN or self.is_superuser
 
     @property
     def is_moderator(self):
-        return self.role == self.UserRole.MODERATOR
+        return self.role == self.MODERATOR
 
     class Meta:
-        ordering = ['username']
+        ordering = ('username',)
 
     def __str__(self):
         if self.username:
